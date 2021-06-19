e59b3859d3c341b46ce064090e7611124b650b24
B. Petersen <r10s@b44t.com>
2021-06-19 22:16:00 +0200

    move create_multiuser_record to ChatId

--- src/dc_receive_imf.rs
+++ src/dc_receive_imf.rs
@@ -1440,7 +1440,7 @@ async fn create_or_lookup_group(
             return Ok((ChatId::new(0), Blocked::Not));
         }
 
-        chat_id = create_multiuser_record(
+        chat_id = ChatId::create_multiuser_record(
             context,
             Chattype::Group,
             &grpid,
@@ -1656,7 +1656,7 @@ async fn create_or_lookup_mailinglist(
 
     if allow_creation {
         // list does not exist but should be created
-        match create_multiuser_record(
+        match ChatId::create_multiuser_record(
             context,
             Chattype::Mailinglist,
             &listid,
@@ -1763,7 +1763,7 @@ async fn create_adhoc_group(
         .get_subject()
         .unwrap_or_else(|| "Unnamed group".to_string());
 
-    let new_chat_id: ChatId = create_multiuser_record(
+    let new_chat_id = ChatId::create_multiuser_record(
         context,
         Chattype::Group,
         &grpid,
@@ -1779,39 +1779,6 @@ async fn create_adhoc_group(
     context.emit_event(EventType::ChatModified(new_chat_id));
 
     Ok(Some(new_chat_id))
-}
-
-async fn create_multiuser_record(
-    context: &Context,
-    chattype: Chattype,
-    grpid: impl AsRef<str>,
-    grpname: impl AsRef<str>,
-    create_blocked: Blocked,
-    create_protected: ProtectionStatus,
-) -> Result<ChatId> {
-    let row_id =
-    context.sql.insert(
-        "INSERT INTO chats (type, name, grpid, blocked, created_timestamp, protected) VALUES(?, ?, ?, ?, ?, ?);",
-        paramsv![
-            chattype,
-            grpname.as_ref(),
-            grpid.as_ref(),
-            create_blocked,
-            time(),
-            create_protected,
-        ],
-    ).await?;
-
-    let chat_id = ChatId::new(u32::try_from(row_id)?);
-    info!(
-        context,
-        "Created group/mailinglist '{}' grpid={} as {}",
-        grpname.as_ref(),
-        grpid.as_ref(),
-        chat_id
-    );
-
-    Ok(chat_id)
 }
 
 /// Creates ad-hoc group ID.
