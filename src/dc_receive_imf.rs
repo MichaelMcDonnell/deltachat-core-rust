98c6a49e299b8a1e6f62d3148f1467cae3e64a2d
B. Petersen <r10s@b44t.com>
2021-06-19 23:06:10 +0200

    always directly return chat-id from dc_join_securejoin()

--- src/dc_receive_imf.rs
+++ src/dc_receive_imf.rs
@@ -1399,9 +1399,26 @@ async fn create_or_lookup_group(
     set_better_msg(mime_parser, &better_msg);
 
     // check, if we have a chat with this group ID
-    let (mut chat_id, _, _blocked) = chat::get_chat_id_by_grpid(context, &grpid)
+    let (mut chat_id, is_protected, _blocked) = chat::get_chat_id_by_grpid(context, &grpid)
         .await
         .unwrap_or((ChatId::new(0), false, Blocked::Not));
+    let create_protected = if mime_parser.get(HeaderDef::ChatVerified).is_some() {
+        if let Err(err) = check_verified_properties(context, mime_parser, from_id, to_ids).await {
+            warn!(context, "verification problem: {}", err);
+            let s = format!("{}. See 'Info' for more details", err);
+            mime_parser.repl_msg_by_error(&s);
+        }
+        ProtectionStatus::Protected
+    } else {
+        ProtectionStatus::Unprotected
+    };
+    if !chat_id.is_unset() && create_protected == ProtectionStatus::Protected && !is_protected {
+        chat_id
+            .inner_set_protection(context, create_protected)
+            .await?;
+        recreate_member_list = true; // TODO: is that needed?
+                                     // TODO: groupname / avatar may also need update
+    }
 
     // check if the group does not exist but should be created
     let group_explicitly_left = chat::is_group_explicitly_left(context, &grpid)
@@ -1423,18 +1440,6 @@ async fn create_or_lookup_group(
                 || X_MrAddToGrp.is_some() && addr_cmp(&self_addr, X_MrAddToGrp.as_ref().unwrap()))
     {
         // group does not exist but should be created
-        let create_protected = if mime_parser.get(HeaderDef::ChatVerified).is_some() {
-            if let Err(err) = check_verified_properties(context, mime_parser, from_id, to_ids).await
-            {
-                warn!(context, "verification problem: {}", err);
-                let s = format!("{}. See 'Info' for more details", err);
-                mime_parser.repl_msg_by_error(&s);
-            }
-            ProtectionStatus::Protected
-        } else {
-            ProtectionStatus::Unprotected
-        };
-
         if !allow_creation {
             info!(context, "creating group forbidden by caller");
             return Ok((ChatId::new(0), Blocked::Not));
